238ad62ecdaa5d1d6a62197e43ad71fdba41ef8c
sowmyadev-kota <kvsowmyasri21@gmail.com>
2024-07-20 21:21:43 +0530

    added functional programming using stream

--- src/programming/FunctionalStructured.java
+++ src/programming/FunctionalStructured.java
@@ -2,11 +2,12 @@ package programming;
 
 import java.util.List;
 
+
 public class FunctionalStructured {
 public static void main(String args[]) {
-	System.out.print("Structured programming");
+	
 	 printAllNumbersStructured(List.of(12,6,7,4,5,7,8,4));
-//	 printAllNumbersFunctional(List.of(12,6,7,4,5,7,8,4));
+	 printAllNumbersFunctional(List.of(12,6,7,4,5,7,8,4));
 }
 
 public static void printAllNumbersStructured(List<Integer> numbers) {
@@ -15,11 +16,14 @@ public static void printAllNumbersStructured(List<Integer> numbers) {
 	}
 
 }
+private static void print(int number) {
+	
+		System.out.println(number);
+	
 }
-//public static void printAllNumbersFunctional(List<Integer> numbers) {
-//	for(int number:numbers) {
-//		System.out.println(number);
-//	}
-//
-//}
-//}
+public static void printAllNumbersFunctional(List<Integer> numbers) {
+	
+	numbers.stream().forEach(FunctionalStructured::print);
+
+}
+}
