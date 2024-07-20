40a8aefe74a9ea251c5b18805856cc0c6ecdbb89
sowmyadev-kota <kvsowmyasri21@gmail.com>
2024-07-20 21:14:02 +0530

    added structured printing

--- src/programming/FunctionalStructured.java
+++ src/programming/FunctionalStructured.java
@@ -1,7 +1,25 @@
 package programming;
 
+import java.util.List;
+
 public class FunctionalStructured {
 public static void main(String args[]) {
 	System.out.print("Structured programming");
+	 printAllNumbersStructured(List.of(12,6,7,4,5,7,8,4));
+//	 printAllNumbersFunctional(List.of(12,6,7,4,5,7,8,4));
+}
+
+public static void printAllNumbersStructured(List<Integer> numbers) {
+	for(int number:numbers) {
+		System.out.println(number);
+	}
+
 }
 }
+//public static void printAllNumbersFunctional(List<Integer> numbers) {
+//	for(int number:numbers) {
+//		System.out.println(number);
+//	}
+//
+//}
+//}
